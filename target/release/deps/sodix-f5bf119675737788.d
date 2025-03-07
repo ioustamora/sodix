8e1f359e21cd87ea0a65ff80468420cfc4c9d170
stan <stanislav@inspeere.com>
2025-03-07 16:47:06 +0100

    install

---
+++ target/release/deps/sodix-f5bf119675737788.d
@@ -0,0 +1,5 @@
+/media/user/btfs/utils/sodix/target/release/deps/sodix-f5bf119675737788: src/main.rs
+
+/media/user/btfs/utils/sodix/target/release/deps/sodix-f5bf119675737788.d: src/main.rs
+
+src/main.rs:
